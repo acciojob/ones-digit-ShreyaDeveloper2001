7afa2dbbf370eee119e18f6e5d708a629621e18e
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-07 20:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function onesDigit(n) {
   // your code here
+	// Check if the input is a positive integer
+  if (typeof n !== 'number' || n <= 0 || !Number.isInteger(n)) {
+    throw new Error("Input is not a positive integer");
+  }
+
+  // Extract the ones digit by taking the remainder when divided by 10
+  const ones = n % 10;
+
+  return ones;
 }
 
 // Do not change the code below
